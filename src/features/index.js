30adcdf68faafe23bcfa691b3bb7a89142d6f047
ThangBC <buicongthang4821@gmail.com>
2022-03-07 00:17:04 +0700

    first add feature home

--- src/features/index.js
+++ src/features/index.js
@@ -1,15 +1,3 @@
-import HelloScreen from './Hello/screens/HelloScreen';
-import LoginScreen from './Login/screens/LoginScreen';
-import RegisterScreen from './Register/screens/RegisterScreen';
 import HomeScreen from './Home/screens/HomeScreen';
-import AddTransactionScreen from './AddTransaction/screens/AddTransactionScreen';
-import ProfileScreen from './Profile/screens/ProfileScreen';
 
-export {
-  HelloScreen,
-  LoginScreen,
-  RegisterScreen,
-  HomeScreen,
-  AddTransactionScreen,
-  ProfileScreen,
-};
+export {HomeScreen};
