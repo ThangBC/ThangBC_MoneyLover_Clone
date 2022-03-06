19127d663dbad59f6ce70838d492c85cf4cc41bc
ThangBC <buicongthang4821@gmail.com>
2022-03-07 00:20:06 +0700

    first add feature add transaction

--- src/features/Home/index.js
+++
@@ -1,5 +0,0 @@
-import FirstPage from './components/FirstPage';
-import SecondPage from './components/SecondPage';
-import ThirdPage from './components/ThirdPage';
-
-export {FirstPage, SecondPage, ThirdPage};
