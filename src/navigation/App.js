094d4b041d51e4bfd7f245dc5bf83901cecd27de
ThangBC <buicongthang4821@gmail.com>
2022-03-07 00:06:40 +0700

    first add feature register

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {
-  HelloScreen,
-  LoginScreen,
-  RegisterScreen,
-  HomeScreen,
-  AddTransactionScreen,
-} from '../features/';
-import UITab from './UITab';
+import {RegisterScreen} from '../features/';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,17 +9,9 @@ const App = props => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="HelloScreen"
+        initialRouteName="RegisterScreen"
         screenOptions={{headerShown: false}}>
-        <Stack.Screen name="HelloScreen" component={HelloScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen
-          name="AddTransactionScreen"
-          component={AddTransactionScreen}
-        />
-        <Stack.Screen name="UITab" component={UITab} />
       </Stack.Navigator>
     </NavigationContainer>
   );
