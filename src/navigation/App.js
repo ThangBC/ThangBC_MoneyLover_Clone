eb864c06dd4854dc00fe128df3f05d77530161a2
ThangBC <buicongthang4821@gmail.com>
2022-03-18 17:08:12 +0700

    fix UI Home

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -17,7 +17,7 @@ const App = props => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LoginScreen"
+        initialRouteName="UITab"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="HelloScreen" component={HelloScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
