3c983059e84328c365188a0a574c59580c5083d3
ThangBC <buicongthang4821@gmail.com>
2022-03-21 21:22:11 +0700

    continue code get data

--- src/features/Home/components/FirstPage.js
+++ src/features/Home/components/FirstPage.js
@@ -9,110 +9,109 @@ const FirstPage = props => {
   const {navigate, goBack} = navigation;
 
   const [tien, setTien] = useState([]);
-  console.log(tien);
+  console.log(`Kết quả ${JSON.stringify(tien)}`);
 
   useEffect(() => {
-    getDocs(collection(db, 'transaction')).then(snapshot => {
-      snapshot.docs.forEach(async doc => {
-        if (doc.data().createdById == auth.currentUser?.uid) {
-          setTien(prev => [...prev, doc.data()]);
-        }
-      });
-    });
-  }, []);
+    const getList = async () => {
+      const data = await getDocs(collection(db, 'transaction'));
+      const result = data.docs.map(detail => ({
+        ...detail.data(),
+        id: detail.id,
+      }));
+      setTien(result);
+    };
+    getList();
+  }, [db]);
 
   return (
     <View style={{flex: 1}}>
-      <ScrollView>
-        <View style={{backgroundColor: 'white', padding: 10, elevation: 2}}>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginBottom: 5,
-            }}>
-            <Text style={{color: 'gray', fontSize: fontSizes.h3}}>
-              Tiền vào
-            </Text>
-            <Text
-              style={{
-                color: colors.collectColor,
-                fontSize: fontSizes.h3,
-                fontWeight: 'bold',
-              }}>
-              20,000,000
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginBottom: 5,
-            }}>
-            <Text style={{color: 'gray', fontSize: fontSizes.h3}}>Tiền ra</Text>
-            <Text
-              style={{
-                color: colors.spentColor,
-                fontSize: fontSizes.h3,
-                fontWeight: 'bold',
-              }}>
-              2,000,000
-            </Text>
-          </View>
-          <View
-            style={{
-              height: 1,
-              backgroundColor: 'gray',
-              width: '40%',
-              alignSelf: 'flex-end',
-            }}
-          />
+      <View style={{backgroundColor: 'white', padding: 10, elevation: 2}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 5,
+          }}>
+          <Text style={{color: 'gray', fontSize: fontSizes.h3}}>Tiền vào</Text>
           <Text
             style={{
-              color: 'black',
+              color: colors.collectColor,
               fontSize: fontSizes.h3,
               fontWeight: 'bold',
-              alignSelf: 'flex-end',
-              marginTop: 5,
             }}>
-            18,000,000
+            20,000,000
           </Text>
-          <View
-            style={{flexDirection: 'row', alignSelf: 'center', marginTop: 20}}>
-            <TouchableOpacity
-              onPress={() => {
-                alert('Tính năng đang được phát triển');
-              }}
-              style={{
-                backgroundColor: 'rgba(255,140,0,0.1)',
-                borderRadius: 30,
-              }}>
-              <Text
-                style={{
-                  color: colors.primaryColor,
-                  padding: 10,
-                  fontSize: fontSizes.h4,
-                }}>
-                Xem báo cáo cho giai đoạn này
-              </Text>
-            </TouchableOpacity>
-          </View>
         </View>
-        <FlatList
-          data={tien}
-          renderItem={(index, item) => {
-            return (
-              <ItemExpenseTracker
-                index={index}
-                key={index}
-                onPress={() => {
-                  navigate('ShowDetailTransScreen');
-                }}
-              />
-            );
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 5,
+          }}>
+          <Text style={{color: 'gray', fontSize: fontSizes.h3}}>Tiền ra</Text>
+          <Text
+            style={{
+              color: colors.spentColor,
+              fontSize: fontSizes.h3,
+              fontWeight: 'bold',
+            }}>
+            2,000,000
+          </Text>
+        </View>
+        <View
+          style={{
+            height: 1,
+            backgroundColor: 'gray',
+            width: '40%',
+            alignSelf: 'flex-end',
           }}
         />
-      </ScrollView>
+        <Text
+          style={{
+            color: 'black',
+            fontSize: fontSizes.h3,
+            fontWeight: 'bold',
+            alignSelf: 'flex-end',
+            marginTop: 5,
+          }}>
+          18,000,000
+        </Text>
+        <View
+          style={{flexDirection: 'row', alignSelf: 'center', marginTop: 20}}>
+          <TouchableOpacity
+            onPress={() => {
+              alert('Tính năng đang được phát triển');
+            }}
+            style={{
+              backgroundColor: 'rgba(255,140,0,0.1)',
+              borderRadius: 30,
+            }}>
+            <Text
+              style={{
+                color: colors.primaryColor,
+                padding: 10,
+                fontSize: fontSizes.h4,
+              }}>
+              Xem báo cáo cho giai đoạn này
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <FlatList
+        data={tien}
+        renderItem={({item, index}) => {
+          return (
+            <ItemExpenseTracker
+              item={item}
+              index={index}
+              key={index}
+              onPress={() => {
+                navigate('ShowDetailTransScreen');
+              }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
